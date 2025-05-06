dff4bb06f93c0fc182e64cc0757deb14fbe17aec
mirbek10 <mirbekbook@gmail.com>
2025-05-06 12:31:11 +0600

    added group block and education block

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Swiper from '../../Components/Swiper/Swiper'
 import FirstSteps from '../../Components/FirstSteps/FirstSteps'
+import Group from '../../Components/Group/Group'
+import Education from '../../Components/Education/Education'
 
 function Home() {
   return (
     <div>
+        <Education/>
+        <Group/>
         <Swiper/>
         <FirstSteps/>
     </div>
