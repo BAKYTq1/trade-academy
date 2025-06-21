10bde0485540504db22a6cd390fe5ceba3d35371
BAKYTq1 <bsainaev@gmail.com>
2025-06-21 19:28:00 +0600

    add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import FirstSteps from '../../Components/FirstSteps/FirstSteps'
-import Results from '../../components/blockresult/Result'
+import Results from '../../components/block7/Result'
 import Get from '../../Components/get/Get'
 import Course from '../../components/BlockCourse/Courses'
 import About from '../../components/about/Aboutt'
 import Strategies from '../../components/BlockSTRATEGIES/STRATEGIE'
 import Str9 from '../../Components/Section9/Str9'
 import Tarif from '../../components/Tarif/Tariff'
-import ChartSection from '../../components/BlockChartSection/ChartSection'
+import ChartSection from '../../components/Blocck6/ChartSection'
 import Banner from '../../components/Banner/Index'
 
 function Home() {
